38b206304193c9c9688e59a5d10b9f11bec14b9a
German Bobadilla <germanbobadilla@gmail.com>
2023-01-22 19:39:47 -0400

    yt-clone commit

--- yt-clone/src/components/Sidebar.jsx
+++ yt-clone/src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Stack } from '@mui/material';
+
+import { categories } from './utilities/constants';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: 'auto',
+        height: { sx: 'auto', md: '95%' },
+        flexDirection: { md: 'column' }
+      }}
+    >
+      {categories.map((category) => (
+        <button>
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
 };
 
 export default Sidebar;
